229857dd6bca1da157167afa6c43e847fcab85fa
ronliu <ronliu2k@gmail.com>
2017-11-10 22:47:01 +1100

    moved environment back to scm

--- web/src/App.js
+++ web/src/App.js
@@ -28,7 +28,7 @@ const App = () => {
 		<Provider store = {store}>
 		<div className="App">
 			<header className="App-header">
-				<h1 className="App-title">Welcome to Shorten Url   --for nintex</h1>
+				<h1 className="App-title">Welcome to Shorten Url   --for nintex 88</h1>
 			</header>
 			<div className="App-Content">
 				<Shorten/>
